116af5704ab831131fb38d56c09a147e1565af16
Sent1ence <adrbl.iit@gmail.com>
2020-12-14 12:33:31 +0800

    Final Authentication commit

---
+++ mdia-front/src/views/auth/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <AuthTemplate>
+    <template #title> Sign in </template>
+    <template #form>
+      <v-text-field label="username" v-model="username" :rules="usernameRules" />
+      <v-text-field label="password" v-model="password" :rules="passwordRules" />
+    </template>
+    <template #actions>
+      <v-btn @click="submit" color="#3cd1c2" class="white--text">Submit </v-btn>
+    </template>
+  </AuthTemplate>
+</template>
+<script>
+import AuthTemplate from "./AuthTemplate";
+import{  mapActions } from 'vuex'
+export default {
+  name: "Login",
+  components: { AuthTemplate },
+  data() {
+    return {
+      username: "",
+      password: "",
+      usernameRules: [(v) => !!v || "Username is required"],
+      passwordRules: [(v) => !!v || "Password is required"],
+    };
+  },
+  methods: {
+    ...mapActions('auth', ['login']),
+    submit() {
+      const payload = {
+        username: this.username,
+        password: this.password
+      }
+      this.login(payload)
+    }
+  }
+};
+</script>
